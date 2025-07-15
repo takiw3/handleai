591f24e8649edc7481ac84901b08a1b6034d98e7
takiw3 <123529543+takiw3@users.noreply.github.com>
2025-07-15 15:24:22 -0400

    Update Cal.com calendar integration

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -8,36 +8,16 @@ const BookingPage = () => {
     const existingScripts = document.querySelectorAll('script[src*="cal.com"]');
     existingScripts.forEach(script => script.remove());
 
-    // Create and inject the Cal.com embed script
+    // Clean up Cal object
+    if (window.Cal) {
+      delete window.Cal;
+    }
+
+    // Add the new Cal.com embed script
     const script = document.createElement('script');
+    script.type = 'text/javascript';
     script.innerHTML = `
-      (function (C, A, L) { 
-        let p = function (a, ar) { a.q.push(ar); }; 
-        let d = C.document; 
-        C.Cal = C.Cal || function () { 
-          let cal = C.Cal; 
-          let ar = arguments; 
-          if (!cal.loaded) { 
-            cal.ns = {}; 
-            cal.q = cal.q || []; 
-            d.head.appendChild(d.createElement("script")).src = A; 
-            cal.loaded = true; 
-          } 
-          if (ar[0] === L) { 
-            const api = function () { p(api, arguments); }; 
-            const namespace = ar[1]; 
-            api.q = api.q || []; 
-            if(typeof namespace === "string"){
-              cal.ns[namespace] = cal.ns[namespace] || api;
-              p(cal.ns[namespace], ar);
-              p(cal, ["initNamespace", namespace]);
-            } else p(cal, ar); 
-            return;
-          } 
-          p(cal, ar); 
-        }; 
-      })(window, "https://app.cal.com/embed/embed.js", "init");
-      
+      (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; if(typeof namespace === "string"){cal.ns[namespace] = cal.ns[namespace] || api;p(cal.ns[namespace], ar);p(cal, ["initNamespace", namespace]);} else p(cal, ar); return;} p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
       Cal("init", "30min", {origin:"https://app.cal.com"});
 
       Cal.ns["30min"]("inline", {
@@ -98,7 +78,7 @@ const BookingPage = () => {
         {/* Cal.com Inline Embed */}
         <div className="max-w-4xl mx-auto">
           <div className="bg-white rounded-2xl overflow-hidden shadow-2xl p-4">
-            <div style={{width:"100%",height:"700px",overflow:"scroll"}} id="my-cal-inline-30min"></div>
+            <div style={{width:"100%",height:"100%",overflow:"scroll"}} id="my-cal-inline-30min"></div>
           </div>
         </div>
 
